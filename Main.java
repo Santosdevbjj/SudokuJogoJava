e2b75493e85d10f8ca88bb1509ee45931238315d
Sergio <santossergiorealbjj@outlook.com>
2025-08-02 18:36:00 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,57 +1,9 @@
-
 // src/main/Main.java
 
 package main;
 
-import javax.swing.SwingUtilities;
-
-import model.Tabuleiro;
 import view.MenuInicial;
-import view.PainelJogo;
-import controller.ControladorJogo;
-import javax.swing.*;
-
-
-
-
-
-
-
-public class Main {
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            MenuInicial menu = new MenuInicial();
-            menu.setVisible(true);
-
-            // TODO: Ajustar o MenuInicial para chamar a lógica abaixo quando o botão for clicado
-            // Exemplo de como a lógica de inicialização do jogo funcionará:
-            
-            // int tamanhoTabuleiro = 9; // Tamanho escolhido pelo jogador
-            // Tabuleiro tabuleiro = new Tabuleiro(tamanhoTabuleiro);
-            // PainelJogo painelJogo = new PainelJogo(tabuleiro);
-            // ControladorJogo controlador = new ControladorJogo(tabuleiro, painelJogo);
-            
-            // JFrame jogoFrame = new JFrame("Sudoku - Jogo");
-            // jogoFrame.add(painelJogo);
-            // jogoFrame.pack(); // Ajusta o tamanho da janela ao conteúdo
-            // jogoFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            // jogoFrame.setLocationRelativeTo(null);
-            // jogoFrame.addKeyListener(controlador);
-            // jogoFrame.setVisible(true);
-            
-            // Para que o KeyListener funcione, o componente deve ter foco.
-            // painelJogo.requestFocusInWindow();
-        });
-    }
-} 
-
-
-
-
-// FIZ ALTERAÇÃO AQUI
-
-
+import javax.swing.SwingUtilities;
 
 public class Main {
 
@@ -59,6 +11,7 @@ public class Main {
      * O método main inicia a aplicação, chamando o método de inicialização do menu.
      */
     public static void main(String[] args) {
+        // Usa uma lambda para garantir que o código da GUI seja executado na thread de despacho de eventos.
         SwingUtilities.invokeLater(Main::iniciarMenu);
     }
     
@@ -68,13 +21,5 @@ public class Main {
      */
     public static void iniciarMenu() {
         MenuInicial menu = new MenuInicial();
-        menu.setVisible(true);
     }
 }
-
-
-
-
-
-
-
